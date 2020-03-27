1efc39f2d36b1b908d9fd5d41bc84001eab3be1b
Chris <chris_legaxy@yahoo.com>
2020-03-27 23:58:43 +0700

    Complete Magic Ball module

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,58 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[300],
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Center(
+            child: Text(
+              'Ask Me Anything',
+              style: TextStyle(
+                fontFamily: 'Lobster',
+                fontSize: 25.0,
+              ),
+            ),
+          ),
+        ),
+        body: MagicBall(),
       ),
     );
+  }
+}
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int answer = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  answer = Random().nextInt(5) + 1;
+                  print(answer);
+                });
+              },
+              child: Image.asset('images/ball$answer.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
